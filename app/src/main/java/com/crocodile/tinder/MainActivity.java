c6d0f4b3ce31d3ca3230e07b775dceba944bb9c6
Anton Butov <butov6101@gmail.com>
2020-06-01 15:43:05 +0300

    privaci policy

--- app/src/main/java/com/crocodile/tinder/MainActivity.java
+++ app/src/main/java/com/crocodile/tinder/MainActivity.java
@@ -2,13 +2,36 @@ package com.crocodile.tinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView policyTextView;
+    private Button buttonStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        policyTextView = findViewById(R.id.textViewPolic);
+        policyTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String urlPolic = "https://www.freeprivacypolicy.com/";
+                Uri uri = Uri.parse(urlPolic);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+
+
     }
 }
